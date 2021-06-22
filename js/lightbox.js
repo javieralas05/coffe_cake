cf6d1451fc2bc5b4c63c586095e3d406735f9a2d
Javier <Alas>
2021-06-22 16:55:17 -0600

    Added style.css file with design, creating javascript file and preparting POO

---
+++ js/lightbox.js
@@ -0,0 +1,10 @@
+
+// objeto con las propiedades del efecto Lightbox
+var propLighbox = {
+    imgContainer: document.getElementsByClassName('lightbox')
+}
+
+// objeto con las Metodos del efecto Lightbox
+var metLightbox = {
+
+}
