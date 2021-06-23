81867c3ee5d80461fa5a2c1548e4e2960cf63bc2
javier <javier.alas05@gmail.com>
2021-06-22 18:49:34 -0600

    added properties for lightbox

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,10 +1,23 @@
 
 // objeto con las propiedades del efecto Lightbox
-var propLighbox = {
+var propLightbox = {
     imgContainer: document.getElementsByClassName('lightbox')
+    imagen: null
 }
 
 // objeto con las Metodos del efecto Lightbox
 var metLightbox = {
+    inicio: function(){
+        for (var i = 0; i < propLightbox.imgContainer.length; i++){
+            propLightbox.imgContainer[i].addEventListener('click', metLightbox.capturaImagen);
+        }
+    },
 
+    capturaImagen: function(){
+
+        propLightbox.imagen = this;
+        metLightbox.metLightbox(propLightbox,imagen);
+    }
 }
+
+metLightbox.inicio();
